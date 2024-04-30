2f295d5d42556d2a59a603f3d6126e78e43919db
Drunkenpilot <bobhao@gmail.com>
2024-04-30 14:43:39 +0200

    Add definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,58 +1,58 @@
 export interface Printer {
-    // Bluetooth
-    address?: string;
-    bondState?: number;
-    name?: string;
-    type?: number;
-    features?: string[];
+  // Bluetooth
+  address?: string;
+  bondState?: number;
+  name?: string;
+  type?: number;
+  features?: string[];
 
-    // USB
-    productName?: string;
-    manufacturerName?: string;
-    deviceId?: number;
-    serialNumber?: string;
-    vendorId?: number;
+  // USB
+  productName?: string;
+  manufacturerName?: string;
+  deviceId?: number;
+  serialNumber?: string;
+  vendorId?: number;
 }
 
 export interface PrinterToUse {
-    type: 'bluetooth' | 'tcp' | 'usb';
-    id: string | number;
-    address?: string;
-    port?: number;
-    mmFeedPaper?: number;
-    dotsFeedPaper?: number;
-    printerDpi?: number;
-    printerWidthMM?: number;
-    printerNbrCharactersPerLine?: number;
-    charsetEncoding?: {
-        charsetName: string,
-        charsetId: number
-    };
+  type: "bluetooth" | "tcp" | "usb";
+  id: string | number;
+  address?: string;
+  port?: number;
+  mmFeedPaper?: number;
+  dotsFeedPaper?: number;
+  printerDpi?: number;
+  printerWidthMM?: number;
+  printerNbrCharactersPerLine?: number;
+  charsetEncoding?: {
+    charsetName: string;
+    charsetId: number;
+  };
 }
 
 export interface PrintFormattedText extends PrinterToUse {
-    text: string;
+  text: string;
 }
 
 export interface BitmapToHexadecimalString extends PrinterToUse {
-    base64: string;
+  base64: string;
 }
 
 export interface RequestPermissionsResult {
-    granted: boolean;
-    BLUETOOTH?: boolean;
-    BLUETOOTH_ADMIN?: boolean;
-    BLUETOOTH_CONNECT?: boolean;
-    BLUETOOTH_SCAN?: boolean;
+  granted: boolean;
+  BLUETOOTH?: boolean;
+  BLUETOOTH_ADMIN?: boolean;
+  BLUETOOTH_CONNECT?: boolean;
+  BLUETOOTH_SCAN?: boolean;
 }
 
 export interface GetEncodingResult {
-    name: string;
-    command?: string[];
+  name: string;
+  command?: string[];
 }
 
 export interface ErrorResult {
-    error?: string;
+  error?: string;
 }
 
 export interface ThermalPrinterPlugin {
@@ -64,7 +64,11 @@ export interface ThermalPrinterPlugin {
    * @param {function} success
    * @param {function} error
    */
-  listPrinters(data: { type: 'bluetooth' | 'usb'; }, success: (value: Printer[]) => any, error: (value: ErrorResult) => void): void;
+  listPrinters(
+    data: { type: "bluetooth" | "usb" },
+    success: (value: Printer[]) => any,
+    error: (value: ErrorResult) => void
+  ): void;
 
   /**
    * Print a formatted text and feed paper
@@ -81,7 +85,11 @@ export interface ThermalPrinterPlugin {
    * @param {function} success
    * @param {function} error
    */
-  printFormattedText(data: PrintFormattedText, success: () => void, error: (value: ErrorResult) => void): void;
+  printFormattedText(
+    data: PrintFormattedText,
+    success: () => void,
+    error: (value: ErrorResult) => void
+  ): void;
 
   /**
    * Print a formatted text, feed paper and cut the paper
@@ -98,7 +106,11 @@ export interface ThermalPrinterPlugin {
    * @param {function} success
    * @param {function} error
    */
-  printFormattedTextAndCut(data: PrintFormattedText, success: () => void, error: (value: ErrorResult) => void): void;
+  printFormattedTextAndCut(
+    data: PrintFormattedText,
+    success: () => void,
+    error: (value: ErrorResult) => void
+  ): void;
 
   /**
    * Get the printer encoding when available
@@ -111,7 +123,11 @@ export interface ThermalPrinterPlugin {
    * @param {function} success
    * @param {function} error
    */
-  getEncoding(data: PrinterToUse, success: (value: GetEncodingResult) => any, error: (value: ErrorResult) => void): void;
+  getEncoding(
+    data: PrinterToUse,
+    success: (value: GetEncodingResult) => any,
+    error: (value: ErrorResult) => void
+  ): void;
 
   /**
    * Close the connection with the printer
@@ -124,7 +140,11 @@ export interface ThermalPrinterPlugin {
    * @param {function} success
    * @param {function} error
    */
-  disconnectPrinter(data: PrinterToUse, success: () => void, error: (value: ErrorResult) => void): void;
+  disconnectPrinter(
+    data: PrinterToUse,
+    success: () => void,
+    error: (value: ErrorResult) => void
+  ): void;
 
   /**
    * Request permissions for USB printers
@@ -137,9 +157,13 @@ export interface ThermalPrinterPlugin {
    * @param {function} success
    * @param {function} error
    */
-  requestPermissions(data: PrinterToUse, success: (value: RequestPermissionsResult) => any, error: (value: ErrorResult) => void): void;
+  requestPermissions(
+    data: PrinterToUse,
+    success: (value: RequestPermissionsResult) => any,
+    error: (value: ErrorResult) => void
+  ): void;
 
-   /**
+  /**
    * Request permissions for Bluetooth
    *
    * @param {Object[]} data - Data object
@@ -150,12 +174,16 @@ export interface ThermalPrinterPlugin {
    * @param {function} success
    * @param {function} error
    */
-  requestBTPermissions(data: PrinterToUse, success: (value: RequestPermissionsResult) => any, error: (value: ErrorResult) => void): void;
+  requestBTPermissions(
+    data: PrinterToUse,
+    success: (value: RequestPermissionsResult) => any,
+    error: (value: ErrorResult) => void
+  ): void;
 
   /**
    * Convert Drawable instance to a hexadecimal string of the image data
-   * Note: Supports only 255px height 
-   * 
+   * Note: Supports only 255px height
+   *
    * @param {Object[]} data - Data object
    * @param {"bluetooth"|"tcp"|"usb"} data.type - List all bluetooth or usb printers
    * @param {string|number} [data.id] - ID of printer to find (Bluetooth: address, TCP: Use address + port instead, USB: deviceId)
@@ -165,5 +193,28 @@ export interface ThermalPrinterPlugin {
    * @param {function} success
    * @param {function} error
    */
-  bitmapToHexadecimalString(data: BitmapToHexadecimalString, success: (value: string) => any, error: (value: ErrorResult) => void): void;
+  bitmapToHexadecimalString(
+    data: BitmapToHexadecimalString,
+    success: (value: string) => any,
+    error: (value: ErrorResult) => void
+  ): void;
+
+  /**
+   * Convert Drawable instance to a hexadecimal string of the image data
+   * Note: Supports only 255px height
+   *
+   * @param {Object[]} data - Data object
+   * @param {"bluetooth"|"tcp"|"usb"} data.type - List all bluetooth or usb printers
+   * @param {string|number} [data.id] - ID of printer to find (Bluetooth: address, TCP: Use address + port instead, USB: deviceId)
+   * @param {string} [data.address] - If type is "tcp" then the IP Address of the printer
+   * @param {number} [data.port] - If type is "tcp" then the Port of the printer
+   * @param {string} data.base64 - Base64 encoded picture string to convert
+   * @param {function} success
+   * @param {function} error
+   */
+  bitmapToHexadecimalStringLarge(
+    data: BitmapToHexadecimalString,
+    success: (value: string[]) => any,
+    error: (value: ErrorResult) => void
+  ): void;
 }
